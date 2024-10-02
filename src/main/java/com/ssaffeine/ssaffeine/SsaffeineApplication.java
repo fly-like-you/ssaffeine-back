b7cb88385d3ecfdd6a253fca048e0062192a6333
fly-like-you <qkrwnsgh71w@gmail.com>
2024-10-02 23:56:57 +0900

    init. 초기 프로젝트 생성 및 설정

---
+++ src/main/java/com/ssaffeine/ssaffeine/SsaffeineApplication.java
@@ -0,0 +1,13 @@
+package com.ssaffeine.ssaffeine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsaffeineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsaffeineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssaffeine/ssaffeine/SsaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaffeine.ssaffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaffeineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
