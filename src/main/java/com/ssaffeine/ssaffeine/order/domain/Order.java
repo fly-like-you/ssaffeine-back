bfd898576f24ee5666d2accf5efd9ca75d97ef7c
fly-like-you <qkrwnsgh71w@gmail.com>
2024-10-09 23:04:09 +0900

    fix. Order 모델 수정

--- src/main/java/com/ssaffeine/ssaffeine/order/domain/Order.java
+++ src/main/java/com/ssaffeine/ssaffeine/order/domain/Order.java
@@ -62,8 +62,3 @@ public class Order {
 	}
 }
 
-enum OrderStatus {
-	ORDER_PENDING,    // 주문 대기 -> 골 달성 대기
-	ORDER_COMPLETE,   // 주문 완료 -> 골 달성 후 입금 완료
-	ORDER_CANCELLED   // 주문 취소 -> 사용자 취소, 미결제 취소, 골 미달성
-}
