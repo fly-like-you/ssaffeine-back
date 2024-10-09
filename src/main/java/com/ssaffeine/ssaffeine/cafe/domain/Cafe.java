6528b45a95818bfda9105dbd6039adde4c8982a7
fly-like-you <qkrwnsgh71w@gmail.com>
2024-10-09 17:16:52 +0900

    feat. 카페 엔티티 추가

--- src/main/java/com/ssaffeine/ssaffeine/cafe/domain/Cafe.java
+++ src/main/java/com/ssaffeine/ssaffeine/cafe/domain/Cafe.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,16 +15,20 @@ import lombok.NoArgsConstructor;
 @Table(name = "cafes")
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Cafe {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cafe_id")
     private Integer cafeId;
 
-    @Column(name = "name", length = 50, nullable = false)
-    private String name;
+    @Column(name = "cafe_name", length = 50)
+    private String cafeName;
 
-    @Column(name = "number", length = 20, nullable = false)
+    @Column(name = "cafe_number", length = 20)
+    @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{4}$", message = "Invalid cafe number format. Expected format: XXX-XXXX-XXXX")
     private String cafeNumber;
+
+    @Column(name = "address", length = 100)
+    private String address;
 }
